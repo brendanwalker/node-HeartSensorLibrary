ef92037c4a7219675f2579b446384f44a808e377
Brendan Walker <brendan@millerwalker.net>
2021-01-20 01:02:40 -0800

    Added JS bindings for first 3 heart sensor library functions * initialize * update * shutdown

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var hslModule = require("./build/Release/heartsensorlibrary.node");
 module.exports = hslModule;
+
+console.log("Initializing HSL");
+hslModule.initialize();
+
+console.log("Updateing HSL");
+hslModule.update();
+
+console.log("shutdown HSL");
+hslModule.shutdown();
--- src/bindings.cpp
+++ src/bindings.cpp
@@ -13,12 +13,44 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
 #include <napi.h>
-#include "node_heartsensorlibrary.h"
 
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return nodehsl::Init(env, exports);
+#include "HSLClient_CAPI.h"
+#include "ClientConstants.h"
+
+Napi::Value Initialize(const Napi::CallbackInfo &info)
+{
+	Napi::Env env = info.Env();
+
+	HSLResult result = HSL_Initialize(HSLLogSeverityLevel::HSLLogSeverityLevel_error);
+
+	return Napi::Boolean::New(env, result == HSLResult_Success);
 }
 
-NODE_API_MODULE(NODE_GYP_MODULE_NAME, InitAll)
+Napi::Value Update(const Napi::CallbackInfo& info)
+{
+	Napi::Env env = info.Env();
+
+	HSLResult result = HSL_Update();
+
+	return Napi::Boolean::New(env, result == HSLResult_Success);
+}
+
+Napi::Value Shutdown(const Napi::CallbackInfo& info)
+{
+	Napi::Env env = info.Env();
+
+	HSLResult result = HSL_Shutdown();
+
+	return Napi::Boolean::New(env, result == HSLResult_Success);
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+	exports.Set("initialize", Napi::Function::New(env, Initialize));
+	exports.Set("update", Napi::Function::New(env, Update));
+	exports.Set("shutdown", Napi::Function::New(env, Shutdown));
+	return exports;
+}
+
+NODE_API_MODULE(heartsensorlibrary, Init)
