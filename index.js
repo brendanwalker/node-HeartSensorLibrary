8449115dfa3a268d8d47798912dcdd312e9ffc5b
Brendan Walker <brendan@millerwalker.net>
2021-02-09 00:57:45 -0800

    Huge refactor of index.js * Refactored HSLConsoleClient class to be JSON data provider for HR signals * Added HSLHttpServer class to be a lightweight HTTP server that streams HR data via Server-Stream-Events (SSE) * Created client index.html that subscribes to SSE and renders PPG data stream in a simple canvas

--- index.js
+++ index.js
@@ -1,95 +1,284 @@
 var hsl = require("./build/Debug/heartsensorlibrary.node");
 module.exports = hsl;
 
-const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
-if (process.stdin.isTTY) {
-  process.stdin.setRawMode(true);
-}
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var baseDirectory = path.join(__dirname, "public");
+var net = require('net');
+var http = require('http');
+// .extend adds a .withShutdown prototype method to the Server object
+require('http-shutdown').extend();
 
-class HSLConsoleClient {
+class HSLSensorClient {
   constructor() {
-    this.keepRunning = false;
-    this.sensorList = null;
-    this.sensor = null;
     this.updateInternal = null;
+    this.sensors = [];
+    this.listenerCallbacks = [];
   }
 
-  fetchFirstSensor() {
-    console.log("var call getSensorList");
-    this.sensorList = hsl.getSensorList();
-    console.log("get senorList");
+  refreshSensorList() {
+    let sensorList = hsl.getSensorList();
 
-    console.log("Found " + this.sensorList.getSensorCount() + " sensors");
-    if (this.sensorList.getSensorCount() > 0) {
-      this.sensor = this.sensorList.getSensor(0);
-      console.log("  Sensor ID: " + this.sensor.getDeviceFriendlyName());
+    this.sensors = [];
+    for (var i = 0; i < sensorList.getSensorCount(); i++) {
+      let sensor = sensorList.getSensor(i);
+
+      if (sensor != null) {
+        this.sensors.push(sensor);
+        if (sensor.hasCapability(hsl.Sensor.StreamFlags_ECGData))
+          sensor.setDataStreamActive(hsl.Sensor.StreamFlags_ECGData, true)
+        else if (sensor.hasCapability(hsl.Sensor.StreamFlags_PPGData))
+          sensor.setDataStreamActive(hsl.Sensor.StreamFlags_PPGData, true)
+        else if (sensor.hasCapability(hsl.Sensor.StreamFlags_HRData))
+          sensor.setDataStreamActive(hsl.Sensor.StreamFlags_HRData, true)
+      }
     }
+  }
+
+  addListener(_this, callback_fn) {
+    this.listenerCallbacks.push(callback_fn.bind(_this));
+  }
+
+  removeListener(callback_fn) {
+    var index = this.listenerCallbacks.indexOf(callback_fn);
+    if (index >= 0) {
+      this.listenerCallbacks.splice(index, 1);
+    }
+  }
+
+  publishSensorECGStream(sensor) {
+    var ecgBufferIter = sensor.getHeartECGBuffer();
+
+    var ecgArray = [];
+    while (ecgBufferIter.isValid()) {
+      ecgArray.push(ecgBufferIter.getECGData());
+      ecgBufferIter.next();
+    }
+
+    if (ecgArray.length > 0) {
+      sensor.flushHeartECGBuffer();
+
+      publishData({ id: sensor.getSensorID(), type: "ecg", stream: ecgArray });
+    }
+  }
+
+  publishSensorPPGStream(sensor) {
+    var ppgBufferIter = sensor.getHeartPPGBuffer();
+
+    var ppgArray = [];
+    while (ppgBufferIter.isValid()) {
+      ppgArray.push(ppgBufferIter.getPPGData());
+      ppgBufferIter.next();
+    }
+
+    if (ppgArray.length > 0) {
+      sensor.flushHeartPPGBuffer();
+
+      this.publishData({ id: sensor.getSensorID(), type: "ppg", stream: ppgArray });
+    }
+  }
+
+  publishSensorHRStream(sensor) {
+    var hrBufferIter = sensor.getHeartRateBuffer();
+
+    var hrArray = [];
+    while (hrBufferIter.isValid()) {
+      hrArray.push(hrBufferIter.getHRData());
+      hrBufferIter.next();
+    }
+
+    if (hrArray.length > 0) {
+      sensor.flushHeartRateBuffer();
+
+      publishData({ id: sensor.getSensorID(), type: "hr", stream: hrArray });
+    }
+  }
+
+  publishData(sensorData) {
+    this.listenerCallbacks.forEach(function (callback_fn) {
+      callback_fn(sensorData);
+    });
   }
 
   update() {
     hsl.update();
 
     if (hsl.hasSensorListChanged()) {
-      console.log("sensor list changed");
-      this.sensor = null;
-      this.fetchFirstSensor();
-
-      if (this.sensor != null) {
-        if (this.sensor.hasCapability(hsl.Sensor.StreamFlags_ECGData))
-          this.sensor.setDataStreamActive(hsl.Sensor.StreamFlags_ECGData, true)
-        else if (this.sensor.hasCapability(hsl.Sensor.StreamFlags_PPGData))
-          this.sensor.setDataStreamActive(hsl.Sensor.StreamFlags_PPGData, true)
-        else if (this.sensor.hasCapability(hsl.Sensor.StreamFlags_HRData))
-          this.sensor.setDataStreamActive(hsl.Sensor.StreamFlags_HRData, true)
-      }
-      else {
-        console.log("No Sensors found. Waiting...");
-      }
+      this.refreshSensorList();
     }
 
-    if (this.sensor != null) {
-      var ppgBufferIter = this.sensor.getHeartPPGBuffer();
-
-      while (ppgBufferIter.isValid()) {
-        var ppgData = ppgBufferIter.getPPGData();
-        var time = ppgData['timeInSeconds'];
-        var samples = ppgData['ppgSamples'];
-
-        samples.forEach(s => {
-          console.log("[0:" + s['ppgValue0'] + ", 1:" + s['ppgValue1'] + ", 2:" + s['ppgValue2'] + "], amb:" + s['ambient'])
-        });
-
-        ppgBufferIter.next();
-      }
-    }
-  }
-
-  shutdown() {
-    if (this.sensor != null) {
-      console.log("Stopping sensor stream");
-      this.sensor.stopAllStreams();
-    }
-
-    clearInterval(this.updateInternal);
-  }
-
-  startup() {
-    console.log("HSL " + hsl.getVersionString());
-
-    process.stdin.on('keypress', (str, key) => {
-      if (key.ctrl && key.name === 'c') {
-        process.exit(); // eslint-disable-line no-process-exit
-      }
+    var _this = this;
+    this.sensors.forEach(function (sensor) {
+      _this.publishSensorECGStream(sensor);
+      _this.publishSensorPPGStream(sensor);
+      _this.publishSensorHRStream(sensor);
     });
   }
 
-  run() {
-    this.startup();
-    var _this = this;
-    this.updateInternal = setInterval(function () { _this.update(); }, 100);
+  start() {
+    if (this.updateInternal == null) {
+      console.log("HSL " + hsl.getVersionString());
+
+      var _this = this;
+      this.updateInternal = setInterval(function () { _this.update(); }, 100);
+    }
+  }
+
+  stop() {
+    if (this.updateInternal != null) {
+      this.sensors.forEach(function (sensor) {
+        sensor.stopAllStreams();
+      });
+
+      clearInterval(this.updateInternal);
+      this.updateInternal = null;
+    }
   }
 }
 
-let hslClient = new HSLConsoleClient();
-hslClient.run()
+class HSLHttpServer {
+  constructor(port) {
+    //  For the static files we server out of the 
+    this.contentTypeByExtension = {
+      '.css': 'text/css',
+      '.gif': 'image/gif',
+      '.html': 'text/html',
+      '.jpg': 'image/jpeg',
+      '.js': 'text/javascript',
+      '.png': 'image/png',
+    };
+
+    this.httpServer = null;
+    this.httpPort = port;
+    this.hslClient = new HSLSensorClient();
+
+    // This array holds the clients (actually http server response objects) to send data to over SSE
+    this.clients = [];
+  }
+
+  handleServerStreamEventRequest(request, response) {
+    // Return Server-Stream-Event data
+    // http://www.html5rocks.com/en/tutorials/eventsource/basics/
+    var headers = {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive'
+    };
+    response.writeHead(200, headers);
+
+    this.addClient(response);
+  }
+
+  handleStaticContentRequest(request, response) {
+    // Technique modified from this:
+    // http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+    const baseURL = 'http://' + request.headers.host + '/';
+    const requestUrl = new URL(request.url, baseURL);
+
+    // path.normalize prevents using .. to go above the base directory
+    var pathname = path.normalize(requestUrl.pathname);
+
+    // Allows http://localhost:8080/ to be used as the URL to retrieve the main index page
+    if (pathname == '/' || pathname == '\\') {
+      pathname = 'index.html';
+    }
+
+    // Handle static file like index.html and main.js
+
+    // Include an appropriate content type for known files like .html, .js, .css
+    var headers = {};
+    var contentType = this.contentTypeByExtension[path.extname(pathname)];
+    if (contentType) {
+      headers['Content-Type'] = contentType;
+    }
+
+    // path.normalize prevents using .. to go above the base directory above, so
+    // this can only serve files in the public directory
+    var fsPath = path.join(baseDirectory, pathname);
+    var fileStream = fs.createReadStream(fsPath);
+
+    response.writeHead(200, headers);
+    fileStream.pipe(response);
+    fileStream.on('error', function (e) {
+      response.writeHead(404);
+      response.end();
+    });
+  }
+
+  handleRequest(request, response) {
+    try {
+      // Technique modified from this:
+      // http://stackoverflow.com/questions/6084360/using-node-js-as-a-simple-web-server
+      const baseURL = 'http://' + request.headers.host + '/';
+      const requestUrl = new URL(request.url, baseURL);
+
+      // path.normalize prevents using .. to go above the base directory
+      var pathname = path.normalize(requestUrl.pathname);
+
+      if (pathname == '/data' || pathname == '\\data') {
+        this.handleServerStreamEventRequest(request, response)
+      }
+      else {
+        this.handleStaticContentRequest(request, response)
+      }
+    } catch (e) {
+      response.writeHead(500);
+      response.end();
+      console.log(e.stack);
+    }
+  }
+
+  addClient(client) {
+    this.clients.push(client);
+  }
+
+  removeClient(client) {
+    var index = this.clients.indexOf(client);
+    if (index >= 0) {
+      this.clients.splice(index, 1);
+    }
+  }
+
+  // Send data to all SSE web browser clients. data must be a string.
+  handleSensorData(data) {
+    var failures = [];
+
+    this.clients.forEach(function (client) {
+      let dataStr = JSON.stringify(data)
+      if (!client.write('data: ' + dataStr + '\n\n')) {
+        failures.push(client);
+      }
+    });
+
+    var _this = this;
+    failures.forEach(function (client) {
+      console.log("[ERROR] Stopping server stream event");
+      _this.removeClient(client);
+      client.end();
+    });
+  }
+
+  start() {
+    var _this = this;
+    this.httpServer = http.createServer(function (request, response) {
+      _this.handleRequest(request, response);
+    }).withShutdown();
+    this.httpServer.listen(this.httpPort);
+
+    this.hslClient.addListener(this, this.handleSensorData);
+    this.hslClient.start();
+  }
+
+  stop() {
+    var _this = this;
+    this.httpServer.shutdown(function () {
+      _this.hslClient.stop();
+      _this.httpServer = null;
+    });
+  }
+}
+
+console.log('Started server at http://localhost:8090');
+let hslHttpServer = new HSLHttpServer(8090);
+hslHttpServer.start()
